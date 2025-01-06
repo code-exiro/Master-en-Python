d9393a7e672e45767ac95890fd04c2261a7dc530
erizo <nandezemir@gmail.com>
2025-01-05 23:50:48 -0600

    Termine el ejercicio 1

--- master-python/Ejercicios2/21.0-ejercicio1.py
+++ master-python/Ejercicios2/21.0-ejercicio1.py
@@ -1,31 +1,56 @@
-"""lista = [1,3,5,7,8,9,3,5]
+'''Ejercicio 1. Hacer un programa que tenga una lista de 8 numeros enteros y haga lo siguiente:
+    -Recorrer la lista y mostrarla 
+    -Hacer una funcion que recorra listas de numeros y devuelva un string
+    -Ordenarla y mostrarla
+    -Mostrar su longitud
+    -Buscar algun elemento (que el usuario pida por teclado)'''
 
-print(len(lista))
-print(lista)
+lista = []
 
-if len(lista) == 8:
-    print("La lista tiene 8 caracteres")
-else:
-    print("algo estamos haciendo mal")
-
-numeroExtra = int(input('Ingresa un numero mas a la lista: '))
-lista.append(numeroExtra)
-
-print(len(lista))
-print(lista)
-
-if len(lista) == 8:
-    print("La lista tiene 8 caracteres")
-else:
-    print("igual y no estamos tan mal")
-"""
-numeros = [13, 45, 34, 34, 34, 23, 92, 67]
-
-def mostrarLista(lista):
+def recorrer_lista(lista):
     resultado = ""
+    for i in range(len(lista)):
+        resultado += f'Elemento {i+1}: {lista[i]}, '
+    return resultado.strip(', ')
 
-    for elemento in lista:
-        resultado += str(elemento)
-        resultado += "\n"
+def buscar_elemento(lista):
+    elemento_buscado = int(input("¿Qué número deseas buscar en la lista? "))
+    if elemento_buscado in lista:
+        posicion = lista.index(elemento_buscado) + 1
+        return f"El número {elemento_buscado} está en la posición {posicion}."
+    else:
+        return f"El número {elemento_buscado} no está en la lista."
 
-print(' ###########  ')
+# Llenar la lista con 8 números
+for numeros in range(8):
+    numeroExtra = int(input('Ingresa un número entero: '))
+    lista.append(numeroExtra)
+
+# Mostrar los números ingresados
+print('\nTus Números son:')
+for i in range(len(lista)):
+    print(f'Número {i+1}: {lista[i]}')
+
+# Ordenar y mostrar la lista
+print('\nLos números ordenados quedan así:')
+lista.sort()
+print(lista)
+
+# Mostrar la cantidad de números
+print(f'Son un total de {len(lista)} números.')
+
+# Mostrar los números de la lista como string
+print('\nLista recorrida funcion:')
+print(recorrer_lista(lista))
+
+# Buscar un número en la lista
+print('\nResultado de la búsqueda:')
+print(buscar_elemento(lista))
+
+'''Que aprendi de este ejercicio?
+
+Me di cuenta de que necesito mejorar mi capacidad para analizar y discernir los problemas que se me presentan. Durante esta tarea, me compliqué demasiado pensando en lo que me pedían, cómo me lo pedían y en cuál sería la mejor manera de implementarlo.
+
+Creo que pude haber abordado la solución de una manera más eficiente. Por ejemplo, si hubiera comentado el código desde el principio, habría tenido más claridad sobre lo que hacía cada parte del programa, lo que también me habría facilitado hacer ajustes y correcciones.
+
+A pesar de haber terminado, el resultado final no me deja satisfecho. Siento que podría haber tomado un enfoque diferente. Es importante organizar mis ideas antes de comenzar a desarrollar, priorizar la claridad y documentar mi trabajo desde el inicio.'''
